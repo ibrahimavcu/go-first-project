dc763ef23e532f254fab1f265f1ff2cb0f571f94
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-19 21:02:23 +0300

    config init

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"berkantbegdilili.com/first-project/config"
 	"berkantbegdilili.com/first-project/routers"
 )
 
 func main() {
+	config.InitConfig()
 	r := routers.InitRouters()
 	r.Run(":3000")
 }
