fbfa39867524dccb89d59c87407df9a91fce2cc2
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2022-02-19 21:03:26 +0300

    findById func have been added.

--- routers/api/v1/choice/controller.go
+++ routers/api/v1/choice/controller.go
@@ -1,7 +1,24 @@
 package choice
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
 
-func register(c *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
 
+func findById(c *gin.Context) {
+	id := c.Param("id")
+
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	var ch choice
+	ch.getOneById(uint(idInt))
+
+	c.JSON(http.StatusOK, ch)
 }
